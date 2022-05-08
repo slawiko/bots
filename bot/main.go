04969b7c646955e76dccc63a397929522b74e1bc
slawiko <shchaurouski.slava@gmail.com>
2022-05-08 17:03:41 +0200

    move skarnik related code in separate file

--- bot/main.go
+++ bot/main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 	"strings"
 
-	"github.com/PuerkitoBio/goquery"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
@@ -116,69 +111,4 @@ func handleCommand(bot *tgbotapi.BotAPI, update *tgbotapi.Update) {
 	}
 
 	sendMsg(bot, msg)
-}
-
-type Suggestion struct {
-	Id    int    `json:"id"`
-	Label string `json:"label"`
-}
-
-func cleanTerm(searchTerm string) string {
-	cleanSearchTerm := strings.ReplaceAll(searchTerm, "ў", "щ")
-	cleanSearchTerm = strings.ReplaceAll(cleanSearchTerm, "і", "и")
-	cleanSearchTerm = strings.ReplaceAll(cleanSearchTerm, "’", "ъ")
-	cleanSearchTerm = strings.ReplaceAll(cleanSearchTerm, "'", "ъ")
-
-	return cleanSearchTerm
-}
-
-func translate(searchTerm string) string {
-	cleanSearchTerm := cleanTerm(searchTerm)
-
-	requestUrl := fmt.Sprintf("https://www.skarnik.by/search_json?term=%s&lang=rus", cleanSearchTerm)
-
-	resp, err := http.Get(requestUrl)
-	if err != nil {
-		log.Println(err)
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Println(err)
-	}
-
-	var suggestions []Suggestion
-	json.Unmarshal(body, &suggestions)
-
-	if len(suggestions) == 0 {
-		return EmptyResultMessage
-	}
-
-	requestUrl = fmt.Sprintf("https://www.skarnik.by/rusbel/%d", suggestions[0].Id)
-
-	resp, err = http.Get(requestUrl)
-	if err != nil {
-		log.Println(err)
-	}
-
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	if err != nil {
-		log.Println(err)
-	}
-
-	section := doc.Find("#trn")
-
-	translation := ""
-
-	section.Find("font[color=\"831b03\"]").Each(func(i int, s *goquery.Selection) {
-		if i == 0 {
-			translation += s.Text()
-		} else {
-			translation += fmt.Sprintf(", %s", s.Text())
-		}
-	})
-
-	log.Println(translation)
-
-	return translation
 }
---
+++ bot/skarnik.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+type Suggestion struct {
+	Id    int    `json:"id"`
+	Label string `json:"label"`
+}
+
+func cleanTerm(searchTerm string) string {
+	cleanSearchTerm := strings.ReplaceAll(searchTerm, "ў", "щ")
+	cleanSearchTerm = strings.ReplaceAll(cleanSearchTerm, "і", "и")
+	cleanSearchTerm = strings.ReplaceAll(cleanSearchTerm, "’", "ъ")
+	cleanSearchTerm = strings.ReplaceAll(cleanSearchTerm, "'", "ъ")
+
+	return cleanSearchTerm
+}
+
+func translate(searchTerm string) string {
+	cleanSearchTerm := cleanTerm(searchTerm)
+
+	requestUrl := fmt.Sprintf("https://www.skarnik.by/search_json?term=%s&lang=rus", cleanSearchTerm)
+
+	resp, err := http.Get(requestUrl)
+	if err != nil {
+		log.Println(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+	}
+
+	var suggestions []Suggestion
+	json.Unmarshal(body, &suggestions)
+
+	if len(suggestions) == 0 {
+		return EmptyResultMessage
+	}
+
+	requestUrl = fmt.Sprintf("https://www.skarnik.by/rusbel/%d", suggestions[0].Id)
+
+	resp, err = http.Get(requestUrl)
+	if err != nil {
+		log.Println(err)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		log.Println(err)
+	}
+
+	section := doc.Find("#trn")
+
+	translation := ""
+
+	section.Find("font[color=\"831b03\"]").Each(func(i int, s *goquery.Selection) {
+		if i == 0 {
+			translation += s.Text()
+		} else {
+			translation += fmt.Sprintf(", %s", s.Text())
+		}
+	})
+
+	log.Println(translation)
+
+	return translation
+}
